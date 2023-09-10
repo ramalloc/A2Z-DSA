50119e23fa1fd2fbac50287717f851a7f8f186e7
ramalloc <roshangupta1887@gmail.com>
2023-09-10 23:52:27 +0530

    Updating Pascal Triangle in Step 3.3

--- Step-3/Step-3.3/pascalTriangle.cpp
+++ Step-3/Step-3.3/pascalTriangle.cpp
@@ -1,37 +1,75 @@
 // Pascal Triangle
 
-
 // There can be 3 types of questions :-
 /*
-1.Give Element and Row and Column index;
+1.Give Element at a given Row and Column index;
+2.Print the nth row of Pascale's Triangle
+3. Print the triangle to the nth row
 */
 
-#include <bits/stdc++.h>
+// 1.
+//  The first solution is brute force solution, that generate the whole triangle and get the element at the given index
+//  And we have a Formula for this, which we implement with the use Combination.
+//  Formula is :-  ⁽ᴿ⁻¹⁾ C ₍c-₁₎ .
+/*  ⁿCᵣ =   n!
+         ----------
+         r! X (n-r)!
+*/
+// The above formula is too long and will take much time, So we will observe the formula again by example :-
+/*
+n = 7;
+r = 2;
+for ⁷C₂ =   7!
+         ----------
+         2! X (7-2)!
+
+    =>      7x6x(5)!
+         ---------------   
+            2! X (5)!
+    
+                7x6
+     =>    -----------  => 
+                2! 
+*/
+// 1.So we can see here that 5! is cutting from numerator and denominator.
+// 2. We can also see that 7! is going to numberof iteration of r.
+/* -> Impt. thing to take care =>      7x6
+                                    -----------  
+                                        2x1
+
+                            =>          7     6
+                                       --- X ---
+                                        1     2 
+*/
+
+/*
+#include<iostream>
 using namespace std;
 
-int nCr(int n, int r) {
-    long long res = 1;
+int nCr(int n, int r){
+    int res = 1;
 
-    // calculating nCr:
-    for (int i = 0; i < r; i++) {
-        res = res * (n - i);
-        res = res / (i + 1);
+    for(int i = 0; i < r; i++){
+        res = res * (n-i);
+        res = res / (i+1);
     }
+
     return res;
 }
 
-int pascalTriangle(int r, int c) {
-    int element = nCr(r - 1, c - 1);
-    return element;
-}
 
-int main()
-{
-    int r = 5; // row number
-    int c = 3; // col number
-    int element = pascalTriangle(r, c);
-    cout << "The element at position (r,c) is: "
-            << element << "n";
+int main(){
+    // Use long long, So that answer cannot overflow
+    long long n = 3;
+    long long r = 2;
+
+    long long result = nCr(n,r);
+    cout << result;
+
+
     return 0;
 }
-        
+*/ 
+
+// Time Complexity -> O(r)
+// Space Complexity -> O(1)
