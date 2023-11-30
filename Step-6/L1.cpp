6ee00556fef1bd7b8ef1b48002889475c9df18d1
ramalloc <roshangupta1887@gmail.com>
2023-11-30 17:05:46 +0530

    Done With Insertion and Deletion in Singly List from Striver's Playlist

--- Step-6/L1.cpp
+++ Step-6/L1.cpp
@@ -15,33 +15,43 @@ public:
         this->data = data;
         this->next = NULL;
     };
+
 };
 
-void insertionAtHead(Node *&head, int value)
-{
-    Node *block = new Node(value);
+// Node *insertionAtHead(Node *&head, int value)
+// {
+//     return new Node(value, head);
+// };
+Node *insertionAtHead(Node *&head, int value)
+{   
+    Node* block = new Node(value);
     block->next = head;
     head = block;
+    return head;
 };
 
-void insertionAtTail(Node *&head, int val)
+Node* insertionAtTail(Node *&head, int val)
 {
-    Node *block = new Node(val);
-    Node *ptr = head;
-    while (ptr->next != NULL)
-    {
+   if(head == NULL){
+    Node* block = new Node(val);
+    head = block;
+    return head;
+   }
+    Node* ptr = head;
+    while(ptr->next != NULL){
         ptr = ptr->next;
     }
-    ptr->next = block;
-    block->next = NULL;
+    Node* block = new Node(val);
+    ptr-> next = block;
+   return head;
 };
 
-void insertionAtIndex(Node *&head, int val, int index)
+Node* insertionAtIndex(Node *&head, int val, int index)
 {
     if (index == 1)
     {
-        insertionAtHead(head, val);
-        return;
+        Node* head1 = insertionAtHead(head, val);
+        return head;
     }
     Node *block = new Node(val);
     Node *ptr = head;
@@ -55,8 +65,8 @@ void insertionAtIndex(Node *&head, int val, int index)
     // By this method our tail gets updated to the new and last node
     if (ptr->next == NULL)
     {
-        insertionAtTail(head, val);
-        return;
+        Node* head1 = insertionAtTail(head, val);
+        return head1;
     }
     block->next = ptr->next;
     ptr->next = block;
@@ -107,6 +117,31 @@ int serchInLl(Node *&head, int num)
     return 0;
 }
 
+Node *insertBeforeValue(Node *head, int x, int val) {
+  if (head == NULL) {
+    return head;
+  }
+  if (head->data == val) {
+    Node *block = new Node(x);
+    block->next = head;
+    head = block;
+    return head;
+  }
+  Node *ptr = head;
+  while (ptr->next != NULL) {
+    if (ptr->next->data == val) {
+      Node *block = new Node(x);
+      block->next = ptr->next;
+      ptr->next = block;
+      break;
+    } else {
+      ptr = ptr->next;
+    }
+  }
+  return head;
+};
+
+
 void printLinkedList(Node *&head)
 {
     Node *temp = head;
@@ -120,25 +155,25 @@ void printLinkedList(Node *&head)
 int main()
 {
 
-    Node *block1 = new Node(10);
+    // Node *block1 = new Node(10);
     // cout << block1->data << endl;
     // cout << block1->next << endl;
 
     // --. Insertion At Head
     // Head Pointed To
-    Node *head = block1;
+    // Node *head = block1;
 
-    insertionAtHead(head, 20);
+    // insertionAtHead(head, 20);
     // printLinkedList(head);
 
     // --. Insertion At Tail
     // Tails pointed to
-    insertionAtTail(head, 40);
+    // Node* head5 = insertionAtTail(head, 40);
     // printLinkedList(head);
 
     // Insertion At Index
-    insertionAtIndex(head, 50, 4);
-    // printLinkedList(head);
+    // Node* head4 = insertionAtIndex(head, 50, 4);
+    // printLinkedList(head4);
 
     // Convert Array into Linked List
     vector<int> arr = {1, 2, 3, 4, 5};
@@ -150,15 +185,20 @@ int main()
     // cout << lenOfLl << endl;
 
     // Searching In Linked List
-    int isData = serchInLl(head1, 6);
-    if (isData)
-    {
-        cout << "Data is present in Linked List..";
-    }
-    else
-    {
-        cout << "Data is not present in Linked List..";
-    }
+    // int isData = serchInLl(head1, 6);
+    // if (isData)
+    // {
+    //     cout << "Data is present in Linked List..";
+    // }
+    // else
+    // {
+    //     cout << "Data is not present in Linked List..";
+    // }
+
+
+    // Insert Before Data
+    Node* head2 = insertBeforeValue(head1, 9, 4);
+    printLinkedList(head2);
 
     return 0;
 }
--- Step-6/L2.cpp
+++ Step-6/L2.cpp
@@ -1,12 +1,169 @@
+// Deletion in Linked List
+
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *convertArrToLl(vector<int> arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *ptr = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *block = new Node(arr[i]);
+        ptr->next = block;
+        ptr = block;
+    }
+    return head;
+};
+
+Node *deleteHead(Node *&head)
+{
+    if (head == NULL)
+        return head;
+
+    Node *ptr = head;
+    head = head->next;
+    delete (ptr);
+
+    return head;
+};
+
+Node *deleteTailOfLl(Node *&head)
+{
+    if (head == NULL || head->next == NULL)
+        return NULL;
+
+    Node *ptr = head;
+    while (ptr->next->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+
+    delete (ptr->next);
+    ptr->next = NULL;
+    return head;
+};
+
+Node *deleteAtIndex(Node *&head, int index)
+{
+    if (head == NULL)
+        return head;
+    if (index == 1)
+    {
+        // Node* head1 = deleteHead(head);
+        // return head1;
+
+        Node *ptr = head;
+        head = head->next;
+        free(ptr);
+        return head;
+    }
+
+    Node *ptr = head;
+    Node *prev = NULL;
+    int count = 0;
+
+    while (ptr != NULL)
+    {
+        count++;
+        if (count == index)
+        {
+            prev->next = prev->next->next;
+            free(ptr);
+            break;
+        }
+        prev = ptr;
+        ptr = ptr->next;
+    }
+
+    return head;
+};
+
+Node *deleteAtValue(Node *&head, int value)
+{
+    if (head == NULL)
+        return head;
+    if (head->data == value)
+    {
+        Node *ptr = head;
+        head = head->next;
+        free(ptr);
+        return head;
+    }
+
+    Node *ptr = head;
+    Node *prev = NULL;
+    while (ptr != NULL)
+    {
+        if (ptr->data == value)
+        {
+            prev->next = prev->next->next;
+            free(ptr);
+            break;
+        }
+        prev = ptr;
+        ptr = ptr->next;
+    }
+
+    return head;
+}
+
+void printLinkedList(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+};
+
+int main()
+{
+    Node *block1 = new Node(10);
+
+    vector<int> arr = {1, 2, 3, 4, 5};
+    Node *head1 = convertArrToLl(arr);
+
+    // Deletion On Head in a Linked list
+    // Node* head = deleteHead(head1);
+    // printLinkedList(head);
+
+    // Deletion On Tail in a Linked list
+    // Node* head = deleteTailOfLl(head1);
+    // printLinkedList(head);
+
+    // Deletion At Index in a Linked list
+    // Node *head2 = deleteAtIndex(head1, 1);
+    // printLinkedList(head2);
 
 
-int main(){
-    
+    // Deletion At Value in a Linked list
+    Node* head4 = deleteAtValue(head1, 3);
+    printLinkedList(head4);
+
+
 
     return 0;
 }
