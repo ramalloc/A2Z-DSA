f137cc796e829519f6319dd9223bad835e652864
ramalloc <roshangupta1887@gmail.com>
2023-12-01 12:18:36 +0530

    Done with the Insertion in Doubly Linked List

--- Step-6/L3.cpp
+++ Step-6/L3.cpp
@@ -138,6 +138,88 @@ void deleteAtNodeInDll(Node *head)
     free(head);
 };
 
+Node *insertBeforeHeadInDll(Node *&head, int val)
+{
+    if (head == NULL)
+    {
+        Node *block = new Node(val);
+        head = block;
+        return head;
+    }
+    Node *block = new Node(val);
+    head->prev = block;
+    block->next = head;
+    head = block;
+    return head;
+};
+
+Node *insertBeforeTailInDll(Node *&head, int val)
+{   
+    if(head->next == NULL){
+        return insertBeforeHeadInDll(head, val);
+    }
+    if (head == NULL)
+    {
+        Node *block = new Node(val);
+        head = block;
+        return head;
+    }
+    Node *ptr = head;
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    Node *block = new Node(val);
+    Node *prevNode = ptr->prev;
+    prevNode->next = block;
+    ptr->prev = block;
+    block->next = ptr;
+
+    return head;
+};
+
+Node* insertBeforeKthInDll(Node* &head, int val, int x){
+    if (head == NULL)
+    {
+        Node *block = new Node(val);
+        head = block;
+        return head;
+    }
+    Node* ptr = head;
+    int count = 0;
+    while(ptr != nullptr){
+        count++;
+        if(count == x){
+            break;
+        }
+        ptr = ptr->next;
+    }
+
+    if(ptr->prev == NULL ||ptr->prev == NULL && ptr->next == NULL){
+        return insertBeforeHeadInDll(head, val);
+    }
+    else if(ptr->next == NULL){
+        return insertBeforeTailInDll(head, val);
+    }
+    Node* prevNode = ptr->prev;
+    Node* block = new Node(val);
+    prevNode->next = block;
+    ptr->prev = block;
+    block->next = ptr;
+    block->prev = prevNode;
+
+    return head;
+};
+
+void insertBeforeNodeInDll(Node* &node, int val){
+    Node* prevNode = node->prev;
+    Node* block = new Node(val);
+    block->prev = prevNode;
+    block->next = node;
+    prevNode->next = block;
+    node->prev = block;
+}
+
 void printDll(Node *head)
 {
     while (head != NULL)
@@ -170,8 +252,28 @@ int main()
     // Deletion at given Node in Doubly Linked List
     // --> Node that given will never be head
     // head->next->next = 3
-    deleteAtNodeInDll(head->next->next);
-    printDll(head);
+    // deleteAtNodeInDll(head->next->next);
+    // printDll(head);
 
-        return 0;
+    // -- Insertion In Doubly Linked List
+
+    // -- Insertion Before Head in Doubly Linked List
+    // Node* head4 = insertBeforeHeadInDll(head, 10);
+    // printDll(head4);
+
+    // -- Insertion Before Tail in Doubly Linked List
+    // Node *head5 = insertBeforeTailInDll(head, 20);
+    // printDll(head5);
+
+
+    // -- Insertion Before kth Node in Doubly Linked List
+    // Node* head6 = insertBeforeKthInDll(head, 30, 5);
+    // printDll(head6);
+
+
+    // -- Insertion Before kth Node in Doubly Linked List
+    // insertBeforeNodeInDll(head->next->next, 40);
+    // printDll(head);
+
+    return 0;
 }
