89437b2b34cdaaf57bd51bffed867423cc6e1b4d
ramalloc <roshangupta1887@gmail.com>
2023-12-07 16:40:08 +0530

    Done with the L8 delete the Nth Node from end by fast and slow pointer method

--- Step-6/L8.cpp
+++ Step-6/L8.cpp
@@ -46,8 +46,70 @@ Node *removeKthNode(Node *head, int K) {
 // Time Complexity = O(len of LL) + O(len-N of LL) ~ O(2N)
 // Space Complexity = O(1)
 
-
-
 // 2. Using two Pointer Nodes Fast and Slow Pointer
+/*
 
+Node *removeKthNode(Node *head, int K) {
+  // Write your code here.
+  Node *dummyHead = new Node(-1);
+  if (head == NULL || head->next == NULL)
+    return dummyHead;
+  // Initialiaze the fast at head
+
+  Node *fast = head;
+  //   Moving the fast to the k steps ahead from head;
+  for (int i = 0; i < K; i++) {
+    fast = fast->next;
+  }
+
+  // If N = 5 means to delete Head
+  Node *headPtr = head;
+  if (fast == NULL) {
+    head = head->next;
+    delete(headPtr);
+    return head;
+  }
+
+  //   Now we make slow which points head and alsways k steps before of fast
+  Node *slow = head;
+  // At last when fast->next == NULL, slow will be pointing to the K+1th Node
+  while (fast->next != NULL) {
+    fast = fast->next;
+    slow = slow->next;
+  }
+  Node *delNode = slow->next;
+  slow->next = slow->next->next;
+  delete (delNode);
+  return head;
+}
+*/
+
+// OR
+
+/*
+Node *removeKthNode(Node *head, int K) {
+  Node *fast = head;
+  Node *slow = head;
+
+  for (int i = 0; i < K; i++)
+    fast = fast->next;
+
+  if (fast == NULL)
+    return head->next;
+
+  while (fast->next != NULL) {
+    fast = fast->next;
+    slow = slow->next;
+  }
+
+  Node *delNode = slow->next;
+  slow->next = slow->next->next;
+  free (delNode);
+  return head;
+}
+
+*/
+
+// Time Complexity = O(N)
+// Space Complexity = O(1)
 
