77f1f6b1768e7ef180e196c7e5a83465aa2b3135
ramalloc <roshangupta1887@gmail.com>
2023-12-08 11:45:17 +0530

    Done with L10 Check the given LL is Palindrome or not by turtoise and Haire Method or SLow and Fast Pointer Method

--- Step-6/L10.cpp
+++ Step-6/L10.cpp
@@ -28,3 +28,60 @@ bool isPalindrome(Node *head)
 
 
 
+// 2. Using Slow and Fast Pointer
+/*  -> We will try to make first half and second half by fast and slow pointer
+    -> Then we will reverse the second half to compare the data with first half data
+    -> After comparing we will reverse the second half again
+*/
+
+/*
+Node *reverseLinkedList(Node *head) {
+  Node *ptr = head;
+  Node *prev = NULL;
+
+  while (ptr != NULL) {
+    Node *front = ptr->next;
+    ptr->next = prev;
+    prev = ptr;
+    ptr = front;
+  }
+  return prev;
+}
+bool isPalindrome(Node *head) {
+  Node *slow = head;
+  Node *fast = head;
+
+  // Moving slow and fast to reach at the mid of LL to separate the LL into two
+  // half.
+  while (fast->next != NULL && fast->next->next != NULL) {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  // Now slow at the end of first half and fast at the start of second half
+  // Now we will reverse the second half
+  Node *newHead = reverseLinkedList(slow->next);
+  // Now we have the starting head of reversed LL in newHead
+
+  // We will make pointers which are pointing to end of first half and at the
+  // newHead of second half
+  Node *first = head;
+  Node *second = newHead;
+
+// Comparing Datas from first and second half
+  while (second != NULL) {
+    if (first->data != second->data) {
+        // reverse the second half LL again before returning the false
+      reverseLinkedList(newHead);
+      return false;
+    }
+    second = second->next;
+    first = first->next;
+  }
+
+  reverseLinkedList(newHead);
+  return true;
+}
+*/
+
+// Time Complexity = O(N/2) + O(N/2) + O(N/2) + O(N/2) ~ O(2N)
+// Space Complexity = O(1)
