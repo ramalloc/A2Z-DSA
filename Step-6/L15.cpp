23073d07b4b189578f12ea227cf9b009c9980989
ramalloc <roshangupta1887@gmail.com>
2023-12-09 17:47:09 +0530

    Done with the L15 Find the length of the loop in LL using Tortoise and Hare Method

--- Step-6/L15.cpp
+++ Step-6/L15.cpp
@@ -24,5 +24,40 @@ int lengthOfLoop(Node *head) {
   }
   return 0;
 }
-
 */
+// Time Complexiity = O(2N)
+// Space Complexiity = O(N)
+
+
+// 2. Using Tortoise and Hare
+// We will reach at the Node where slow and fast met and from that node reintialize the fast ffrom that node till that present node
+// kept a count which increases with increament of fast then fast reaches back to that node then return the count as length of loop in LL.
+
+/*
+#include <bits/stdc++.h>
+int findLength(Node *fast) {
+  Node *fastHead = fast;
+  int count = 1;
+  fastHead = fastHead->next;
+  while (fastHead != fast) {
+    count++;
+    fastHead = fastHead->next;
+  }
+  return count;
+}
+int lengthOfLoop(Node *head) {
+  Node *slow = head;
+  Node *fast = head;
+  while (fast != NULL && fast->next !=NULL) {
+    slow = slow->next;
+    fast = fast->next->next;
+    if (slow == fast) {
+      return findLength(fast);
+    }
+  }
+  return 0;
+}
+*/
+
+// Time Complexiity = O(2N)
+// Space Complexiity = O(1)
