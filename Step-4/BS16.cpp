6fe701ff513a61ea975589f7a694756a2e52c330
ramalloc <roshangupta1887@gmail.com>
2023-12-26 13:39:18 +0530

    Done with BS16 Find Kth missing Element by Binary Search and Maths Logic Approach

--- Step-4/BS16.cpp
+++ Step-4/BS16.cpp
@@ -34,3 +34,44 @@ int missingK(vector < int > vec, int n, int k) {
 
 
 
+// OPTIMAL / BINARY SEARCH
+/*
+-> We have to assume that if original numbers wre there, so how they were arranged and that will be our missing number at that index 
+    which is arr[index] + 1.
+-> So we will have to got numbers missing till all the element which is {arr[mid] - [mid+1]};
+-> If missing number is less than k then we will set low = mid + 1;
+-> Else high = mid - 1;
+-> At the end when high crosees low, It can be possible that high points to out of the array.
+-> high points somewhere in the array. Now at that high missing number is arr[high] - (high+1).
+-> And from the arr[high] we need more to find the kth missing element, So the more is (k - missing).
+-> And our missing element is arr[high] + more
+
+Formula
+-> ans = arr[high] + k -(arr[high] - (high+1))
+=> ans = arr[high] + k -(arr[high] - high - 1))
+=> ans = arr[high] + k - arr[high] + high + 1 )   // - arr[high] and + arr[high] cutted off
+=> ans = high + k + 1;
+
+-> We can notice that low = high + 1, So we can replace (high + 1) with low and return low + k.
+*/
+/*
+int missingK(vector<int> vec, int n, int k) {
+  int low = 0;
+  int high = n - 1;
+
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+    int missing = vec[mid] - (mid + 1);
+    if(missing < k){
+        low = mid + 1;
+    }
+    else{
+        high = mid - 1;
+    }
+  }
+
+  return low + k;
+}
+*/
+// Time Complexity = O(N)
+// Space Complexity = O(1)
